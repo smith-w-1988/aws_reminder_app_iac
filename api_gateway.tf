20d095dc6392ffc8eaecadf0499526c41eb77374
smith-w-1988 <smith-w@live.co.uk>
2025-06-27 13:55:48 +0100

    api gateway creation

---
+++ api_gateway.tf
@@ -0,0 +1,34 @@
+resource "aws_apigatewayv2_api" "reminder_api" {
+  name          = "reminder_api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id             = aws_apigatewayv2_api.reminder_api.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = aws_lambda_function.reminder_insert.invoke_arn
+  integration_method = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "post_reminder" {
+  api_id    = aws_apigatewayv2_api.reminder_api.id
+  route_key = "POST /reminder"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.reminder_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+
+resource "aws_lambda_permission" "allow_apigw_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.reminder_insert.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.reminder_api.execution_arn}/*/*"
+}
